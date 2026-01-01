212aaa92b2b141f32733383ee0bf5cb06607ccbe
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2026-01-01 11:18:07 +0100

    working on footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>F</div>
+    <div>
+      {/* decave image */}
+      <div>
+        <h1 className='font-extrabold text-[369px] text-[#121112] drop-shadow-[0_-7px_0_rgba(255,255,255,0.08)]'>
+          deCAVE
+        </h1>      
+      </div>
+    </div>
   )
 }
 
