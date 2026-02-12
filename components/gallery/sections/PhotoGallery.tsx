207d58624a6abcbc8fcc1e3747590f1d92fdd198
nnamdi <uzonnamdi31@gmail.com>
2026-02-12 14:28:41 +0100

    added empty states

--- components/gallery/sections/PhotoGallery.tsx
+++ components/gallery/sections/PhotoGallery.tsx
@@ -6,55 +6,40 @@ import { LuFilter } from "react-icons/lu";
 import { act, useEffect, useState } from "react";
 import TabNavigation from "@/components/layout/TabNavigation";
 import Image from "next/image";
-import { GalleryData, GalleryEventType, getGalleryByEvent, getGalleryEvent } from "@/app/actions/gallery";
+import {
+  GalleryData,
+  GalleryEventType,
+  getGalleryByEvent,
+  getGalleryEvent,
+} from "@/app/actions/gallery";
 
 export default function PhotoGallery() {
   const [activeTab, setActiveTab] = useState("");
-  const [galleryEvents, setGalleryEvents] = useState<GalleryEventType[]>([])
-  const [photoData, setPhotoData] = useState<GalleryData[]>([])
+  const [galleryEvents, setGalleryEvents] = useState<GalleryEventType[]>([]);
+  const [photoData, setPhotoData] = useState<GalleryData[]>([]);
 
   useEffect(() => {
-    const getEvent = async() => {
-      const res = await getGalleryEvent()
-       setGalleryEvents(res)
+    const getEvent = async () => {
+      const res = await getGalleryEvent();
+      setGalleryEvents(res);
 
-      setActiveTab(res[0]._id)
+      setActiveTab(res[0]._id);
+    };
+
+    getEvent();
+  }, []);
+
+  useEffect(() => {
+    const fetchGalleryByEvent = async () => {
+      const res = await getGalleryByEvent(activeTab);
+      setPhotoData(res);
+    };
+
+    if (activeTab) {
+      fetchGalleryByEvent();
     }
+  }, [activeTab]);
 
-    getEvent()
-  }, [])
-
-  useEffect(()=>{
-    
-      const fetchGalleryByEvent = async() => {
-        const res = await getGalleryByEvent(activeTab)
-        setPhotoData(res)
-      }
-
-      if(activeTab){
-
-        fetchGalleryByEvent()
-      }
-  }, [activeTab])
-
-
-  const images = [
-    {id: 1, name: "gallery-img-1.png"}, 
-    {id: 2, name: "gallery-img-2.png"}, 
-    {id: 3, name: "gallery-img-3.png"}, 
-    {id: 4, name: "gallery-img-4.png"}, 
-    {id: 5, name: "gallery-img-5.png"}, 
-    {id: 6, name: "gallery-img-6.png"}
-    ]
-
-  const tabs = [
-    {id: "all", label: "All"},
-    {id: "crowd", label: "Crowd"},
-    {id: "production", label: "Production"},
-    {id: "performance", label: "Performance"},
-    {id: "artists", label: "Artists"},
-    {id: "atmosphere", label: "Atmosphere"},
-  ]
   return (
     <div className="px-4 lg:px-16">
       <SectionHeader
@@ -66,23 +51,26 @@ export default function PhotoGallery() {
       />
 
       {/* Tab Navigation */}
-      <TabNavigation 
+      <TabNavigation
         tabs={galleryEvents}
         activeTab={activeTab}
         onTabChange={setActiveTab}
         showFilter={false}
       />
 
+      {photoData.length === 0 && (
+        <div className="flex justify-center items-center py-10">
+          <p className="text-[#6b6b6b]">No photos found for this event.</p>
+        </div>
+      )}
+
+
       {/* Pictures */}
       <div className="mt-10 w-full grid grid-cols-1 lg:grid-cols-3 gap-4">
         {photoData.map((item) => (
-            <div key={item._id} className="w-full h-100 relative">
-                <Image 
-                    src={item.link}
-                    alt="image"
-                    fill
-                />
-            </div>
+          <div key={item._id} className="w-full h-100 relative">
+            <Image src={item.link} alt="image" fill />
+          </div>
         ))}
       </div>
     </div>
--- components/gallery/sections/VideoArchive.tsx
+++ components/gallery/sections/VideoArchive.tsx
@@ -1,40 +1,41 @@
-"use client"
+"use client";
 
 import SectionHeader from "@/components/layout/sectionHeader";
 import Image from "next/image";
 import VideoCard from "../ui/VideoCard";
 import { galleryVideoData } from "@/lib/data";
-import { GalleryData, getGalleryVideo } from '@/app/actions/gallery';
+import { GalleryData, getGalleryVideo } from "@/app/actions/gallery";
 import { useEffect, useState } from "react";
 import Spinner from "@/components/layout/Spinner";
 
 export default function VideoArchive() {
-    const [videoData, setVideoData] = useState<GalleryData[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
+  const [videoData, setVideoData] = useState<GalleryData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
+  useEffect(() => {
+    const fetchVideoData = async () => {
+      setLoading(true);
+      const res = await getGalleryVideo();
+      console.log("Video res: ", res);
+      setVideoData(res);
 
-    useEffect(() => {
-        const fetchVideoData = async() => {
-            setLoading(true)
-            const res = await getGalleryVideo()
-            setVideoData(res)
+      setLoading(false);
+    };
 
-            setLoading(false)
-        }
+    fetchVideoData();
+  }, []);
 
-        fetchVideoData()
-    }, [])
-    return (
-        <div className="mt-10 px-4 lg:px-16 bg-[#121111] py-10">
-            <SectionHeader 
-                label="VIDEO ARCHIVE"
-                title="Relive the Experience"
-                description="Immersive after-movies and highlights from past editions"
-                align="left"
-            />
+  return (
+    <div className="mt-10 px-4 lg:px-16 bg-[#121111] py-10">
+      <SectionHeader
+        label="VIDEO ARCHIVE"
+        title="Relive the Experience"
+        description="Immersive after-movies and highlights from past editions"
+        align="left"
+      />
 
-            {/* Main  */}
-            {/* <div className="w-full h-100 relative rounded-sm">
+      {/* Main  */}
+      {/* <div className="w-full h-100 relative rounded-sm">
                 <VideoCard 
                     thumbnail="/gallery/video-thumbnail-1.png"
                     videoUrl="#"
@@ -46,26 +47,31 @@ export default function VideoArchive() {
                     imageAlt="video thumbnail"
                 />
             </div> */}
-    {
-        loading? 
-        <div className="w-full flex justify-center">
-                <Spinner/>
-        </div> :
-            <div className="mt-10 w-full grid grid-cols-1 lg:grid-cols-3 gap-3">
-                {videoData.map((item) => (
-                    <VideoCard 
-                        key={item._id}
-                        thumbnail={item.thumbnail ?? "/gallery/gallery-img-3.png"}
-                        videoUrl={item.link}
-                        title={item.event.eventDetails?.eventTitle ?? ""}
-                        year={item.createdAt}
-                        // views={"12k"}
-                        // onPlayClick={item?.onPlayClick}
-                        imageAlt={item.event.eventDetails?.eventTitle ?? "alttext"}
-                    />
-                ))}
-            </div>
-    }
+      {videoData.length === 0 && (
+        <div className="flex justify-center items-center py-10">
+          <p className="text-[#6b6b6b]">No photos found for this event.</p>
         </div>
-    )
+      )}
+      {loading ? (
+        <div className="w-full flex justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="mt-10 w-full grid grid-cols-1 lg:grid-cols-3 gap-3">
+          {videoData.map((item) => (
+            <VideoCard
+              key={item._id}
+              thumbnail={item.thumbnail ?? "/gallery/gallery-img-3.png"}
+              videoUrl={item.link}
+              title={item.event.eventDetails?.eventTitle ?? ""}
+              year={item.createdAt}
+              // views={"12k"}
+              // onPlayClick={item?.onPlayClick}
+              imageAlt={item.event.eventDetails?.eventTitle ?? "alttext"}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
